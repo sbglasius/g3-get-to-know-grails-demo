592ed40029926e35a4bbb8750cec58a79185b497
Søren Berg Glasius <soeren@glasius.dk>
2016-11-30 10:49:32 -0500

    TourData in project

--- grails-app/init/get/to/know/grails/demo/BootStrap.groovy
+++ grails-app/init/get/to/know/grails/demo/BootStrap.groovy
@@ -2,13 +2,14 @@ package get.to.know.grails.demo
 
 import demo.Team
 import groovy.json.JsonSlurper
+import org.grails.io.support.DefaultResourceLoader
 
 class BootStrap {
 
     def init = { servletContext ->
         if (!Team.count()) {
-            def tourData = new File('/Users/sbglasius/Documents/tourData.json')
-            def json = new JsonSlurper().parse(tourData)
+            def tourData = new DefaultResourceLoader().getResource('/tourData.json')
+            def json = new JsonSlurper().parse(tourData.inputStream)
             json.teams.each { Map team ->
                 println "Creating team: $team.name with ${team.riders.size()} riders"
                 def newTeam = new Team(name: team.name.trim(), description: team.description)
